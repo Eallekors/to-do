85d9f2b88de19d5394a5685bf6c7d482df2b8a6a
EAllekors <erkxalle@gmail.com>
2023-10-10 11:08:04 +0300

    Write data to file

--- index.js
+++ index.js
@@ -7,19 +7,49 @@ const path = require("path")
 app.set("view engine","ejs")
 app.set("views", path.join(__dirname, "views"))
 
-app.get("/",(req, res) => {
-	// get data from file
-	fs.readFile("./tasks", "utf8", (err, data) => {
+const readFile = (filename) => {
+	return new Promise((resolve, reject) => {
+		//get data from file
+		fs.readFile("./tasks", "utf8", (err, data) => {
 		if (err) {
 			console.error(err);
 			return;
 		}
-	
 		// task list data
 		const tasks = data.split("\n")
-		res.render("index",{tasks: tasks})
-	});
-	
+		resolve(tasks)
+	})
+})
+}
+
+app.get('/',(req, res) => {
+	// get data from file
+	readFile("./tasks")
+		.then(tasks => {
+			//console.log(tasks)
+			res.render("index", {tasks, tasks})
+		})
+	})
+
+
+// for parsing application / x-www-form-urlencoded
+app.use(express.urlencoded({expanded: true}));
+
+app.post("/", (req, res) => {
+	// get data from file
+	readFile("./tasks")
+		.then(tasks => {
+			tasks.push(req.body.task)
+			const data = tasks.join("\n")
+			fs.writeFile("./tasks", data, err => {
+				if (err) {
+					console.error(err);
+					return;
+				}
+				//redirect to / to see result
+				res.redirect("/")
+			})
+		})
 })
 
 app.listen(3001, () => {
